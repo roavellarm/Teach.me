0134b3afdc98119dc577762e80693a861f13158d
gabrielgrs <grxgabriel@gmail.com>
2017-09-24 14:23:55 -0300

    Criacao component Instructor

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { InstructorComponent } from './instructor/instructor.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    InstructorComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/models/instructor.ts
@@ -0,0 +1,15 @@
+export class Instructor {
+    // id, nome, sobrenome, telefone, nascimento, email, logradouro, cidade, perfil, foto, sexo
+    id: number;
+    firstName: string;
+    lastName: string;
+    phone: number;
+    email: string;
+    street: string;
+    city: string;
+    state: string;
+    country: string;
+    profile: string;
+    // HTMLImageElement
+    avatar: string;
+}
