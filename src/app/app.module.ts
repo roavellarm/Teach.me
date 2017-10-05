f746ee675f0810a74a57e54038812ba042d46124
Gabriel Gonçalves Ribeiro <651200105@fspoaeduc.com.br>
2017-10-05 10:50:15 -0300

    Clean project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,14 +2,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { IndexInstructorComponent } from './index-instructor/index-instructor.component';
-import { FormInstructorComponent } from './form-instructor/form-instructor.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    IndexInstructorComponent,
-    FormInstructorComponent,
+    AppComponent
   ],
   imports: [
     BrowserModule
