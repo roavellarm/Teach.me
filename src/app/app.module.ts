1fc8a2c66b93755b1ae2cb752bafa5acd1b43a55
Gabriel Goncalves Ribeiro <ggribeiro@stefanini.com>
2017-10-11 18:26:04 -0300

    correcao merge

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,8 +6,6 @@ import { RouterModule, Routes } from '@angular/router';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
-<<<<<<< HEAD
-=======
 
 import { IndexInstructorComponent } from './instructor/index-instructor/index-instructor.component';
 import { FormInstructorComponent } from './instructor/form-instructor/form-instructor.component';
@@ -19,11 +17,12 @@ const routes: Routes = [
   { path: 'instructor/:id', component: FormInstructorComponent },
   { path: 'instructor/new', component: FormInstructorComponent },
 ]
->>>>>>> development-instructor
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    IndexInstructorComponent,
+    FormInstructorComponent,
   ],
   imports: [
     BrowserModule,
