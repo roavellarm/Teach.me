0f307a546e834001e3e79aff4241f27cf53af99a
Rodrigo Avellar de Muniagurria <roavellarm@gmail.com>
2017-11-03 12:10:31 -0200

    Fix Navbar - Collapsed Dropdown-toggle button

--- src/app/student/student.service.ts
+++ src/app/student/student.service.ts
@@ -4,12 +4,14 @@ import { Student } from './student';
 @Injectable()
 export class StudentService {
   students:Student[]=[
-    { id:1, firstName:"Beltrano", lastName:"Da Silva", birthDate:new Date(1965,9,15), phone:"(51) 99567-4545", email:"beltrano@email.com",
-      password:"123123", adress:"Onde o diabo perdeu as botas" },
-    { id:2, firstName:"Fulano", lastName:"M", birthDate:new Date(1984,5,30), phone:"(55) 98234-6384", email:"fulano@email.com",
-      password:"321321", adress:"Ohio que o parta" }
+    { id:1, firstName:"Ciclano", lastName:"Araujo", birthDate:new Date(1969,12,24), phone:"(51) 99567-4545", email:"ciclano@email.com",
+      password:"secret", adress:"Além from hell" },
+    { id:2, firstName:"Beltrano", lastName:"Da Silva", birthDate:new Date(1965,9,15), phone:"(51) 99977-7666", email:"beltrano@email.com",
+      password:"secret", adress:"Onde o diabo perdeu as botas" },
+    { id:3, firstName:"Fulano", lastName:"M", birthDate:new Date(1984,5,30), phone:"(55) 98234-6384", email:"fulano@email.com",
+      password:"secret", adress:"Ohio que o parta" }
   ];
-  autoIncrement: number = 3;
+  autoIncrement: number = 4;
   
   constructor() { }
 
