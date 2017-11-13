939eda2ea94ce376665f3f2a6523357c759e1d76
Gabriel Goncalves Ribeiro <ggribeiro@stefanini.com>
2017-11-13 18:15:41 -0200

    add user model

---
+++ server/src/models/user-model.js
@@ -0,0 +1,26 @@
+'use strict';
+
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema({
+    firstName: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true
+    }, 
+    password: {
+        type: String,
+        required: true
+    },
+    roles: [{
+        type: String,
+        required: true,
+        enum: ['admin', 'student', 'instructor']
+    }]
+});
+
+module.exports = mongoose.model('User', userSchema);
