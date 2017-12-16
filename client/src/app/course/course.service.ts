803e7641197672a3d906e9b35b14b9f0bf007ae6
Rodrigo Avellar <roavellarm@gmail.com>
2017-12-16 17:25:52 -0200

    ptade Course.service.ts

--- client/src/app/course/course.service.ts
+++ client/src/app/course/course.service.ts
@@ -1,85 +1,60 @@
-import { User } from './../user/user';
 import { Injectable } from '@angular/core';
 import { Course } from "./course";
-import { CategoryService } from './../category/category.service';
+import { User } from './../user/user';
 import { UserService } from '../user/user.service';
 import { Category } from './../category/category';
-// import { User } from '../user/user';
+import { CategoryService } from './../category/category.service';
+import { Http,  Response, Headers, RequestOptions } from '@angular/http';
+import { Observable } from 'rxjs/RX';
+import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/catch';
 
 @Injectable()
 export class CourseService {
-  // student1: User;
-  // student2: User;
-  // instructor1: User;
-  // instructor2: User;
-  // category1: Category;
-  // category2: Category; 
-  // courses: Course[] = [
-  //   { id: 0,
-  //     title: "Aulas de Inglês",
-  //     // instructor_id: 2,
-  //     // student_id: 5,
-  //     // category_id: 1,
-  //     instructor: this.instructor1,
-  //     student:  this.student1,
-  //     category: this.category1,
-  //     contact: "meuemail@email.com",
-  //     description: "Aulas de Inglês Particular",
-  //     module: true, // true = presencial / false = distancia
-  //     price: 60.50
-  //   },
-  //   { id: 1,
-  //     title: "Aulas de Violão",
-  //     // instructor_id: 4,
-  //     // student_id: 3,
-  //     // category_id: 2,
-  //     instructor: this.instructor2,
-  //     student: this.student2,
-  //     category: this.category2,
-  //     contact: "por email",
-  //     description: "Aulas de violão para iniciantes",
-  //     module: true, // true = presencial / false = distancia
-  //     price: 75.00
-  //   }
-  // ];
-  // // students: User[] = []; NAO APAGAR!!!!!!!!!
-  // // instructors: User[] = [];
-  // // categories: Category[] = [];
   courses: Course[] = [];
-  autoIncrement: number = 1;
+  uri = "http://localhost:3000/course";
 
-  constructor(private userService: UserService, private categoryService: CategoryService) { 
-    // this.instructor1 = userService.get(2); 
-    // this.instructor2 = userService.get(4); 
-    // this.student1 = userService.get(5); 
-    // this.student2 = userService.get(3); 
-    // this.category1 = categoryService.get(1);
-    // this.category2 = categoryService.get(2);
+  constructor(private http: Http) {}
+
+  getAll(): Observable<Course[]> {
+    return this.http.get(this.uri)
+      .map((res: Response) => res.json())
+      .catch((error: any) => Observable.throw(error));
   }
 
-  getAll() {
-    return this.courses;
+  get(id: number): Observable<Course> {
+    let url = this.uri + '/' + id;
+    return this.http.get(url)
+      .map((res: Response) => res.json())
+      .catch((error: any) => Observable.throw(error));
   }
 
-  get(_id: number) {
-    return this.courses.find(course => course.id == _id);
+  post(course: Course): Observable<Course> {
+    let bodyString = JSON.stringify(course);
+    let header = new Headers({ "Content-Type": "application/json"});
+    let options = new RequestOptions({ headers: header });
+
+    return this.http.post(this.uri, bodyString, options)
+      .map((res: Response) => {})
+      .catch((error: any) => Observable.throw(error));
   }
 
-  post(_course: Course) {
-    _course.id = this.autoIncrement++;
-    this.courses.push(_course);
+  put(course: Course): Observable<Course> {
+    let url = this.uri + "/" + course.id;
+    let bodyString = JSON.stringify(course);
+    let header = new Headers({ "Content-Type": "application/json" });
+    let options = new RequestOptions({ headers: header });
+
+    return this.http.put(url, bodyString, options)
+      .map((res: Response) => {})
+      .catch((error: any) => Observable.throw(error));
   }
 
-  put(_id: number, _course: Course) {
-    let i = this.courses.indexOf(this.get(_id), 0);
-    this.courses[i] = _course;
-  }
-
-  delete(_course: Course) {
-    let i = this.courses.indexOf(_course, 0);
-    if (i > -1) {
-      this.courses.splice(i, 1);
-    }
+  delete(course: Course): Observable<Course> {
+    let url = this.uri + "/" + course.id;
+    return this.http.delete(url)
+      .map((res: Response) => {})
+      .catch((error: any) => Observable.throw(error));
   }
 
 }
